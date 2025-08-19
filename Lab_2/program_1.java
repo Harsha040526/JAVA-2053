0a154b54591bcf13677c37d4440d9c9f68d6565e
harsha <harshambca24@rvu.edu.in>
2025-08-19 10:48:16 +0530

    Create program_1.java

---
+++ Lab_2/program_1.java
@@ -0,0 +1,16 @@
+class Student {
+    String name;
+    int age;
+
+    void displayInfo() {
+        System.out.println("Name: " + name + " Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Harsha M";
+        s1.age = 19;
+        s1.displayInfo();
+    }
+}
+.java 
